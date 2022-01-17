6c31bfc452823bbe2bb10df8195d583e29e39129
Aleksei Chamin <aleksei_chamin@epam.com>
2022-01-17 22:09:34 +0300

    Add project files.

---
+++ PointOfSaleTerminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PointOfSaleTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
